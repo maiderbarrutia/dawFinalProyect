89650dfbb7392bd5b3634f3d49e4c8a792210c58
Maider <maiderbarrutia@hotmail.com>
2025-03-23 19:08:06 +0100

    update common button

--- frontend/src/components/common/Button/Button.tsx
+++ frontend/src/components/common/Button/Button.tsx
@@ -3,37 +3,41 @@ import styles from './Button.module.scss';
 
 interface ButtonProps {
   text: string;
-  onClick?: () => void;
+  handleClick?: () => void;
   type?: 'button' | 'submit' | 'reset';
   className?: string;
   disabled?: boolean;
   buttonStyle?: 'primaryColor' | 'black' | 'white';
   hoverStyle?: 'primaryColor' | 'black' | 'white';
+  ariaLabel?: string;
 }
 
 const Button: React.FC<ButtonProps> = ({
   text,
-  onClick,
+  handleClick,
   type = 'button',
   className = '',
   disabled = false,
   buttonStyle = 'primaryColor',
   hoverStyle = 'black',
+  ariaLabel,
 }) => {
   const [isHovered, setIsHovered] = useState(false);
 
   // Determina la clase de variante basada en las props
   const buttonStyleClass = styles[`button--${buttonStyle}`];
   const hoverStyleClass = styles[`button--${hoverStyle}`];
+  const ariaLabelValue = ariaLabel || text;
 
   return (
     <button
       type={type}
-      onClick={onClick}
+      onClick={handleClick}
       className={`${styles.button} ${isHovered ? hoverStyleClass : buttonStyleClass} ${className}`}
       disabled={disabled}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
+      aria-label={ariaLabelValue}
     >
       {text}
     </button>
