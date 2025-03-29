da0e91af167c33283657bb118f1ac4a3df8be769
Maider <maiderbarrutia@hotmail.com>
2025-03-29 20:01:47 +0100

    add activities page

---
+++ frontend/src/pages/activitiesPage/ActivitiesPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ActivitiesPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Actividades</h1>
+      <p>Aquí irán todas las actividades.</p>
+    </div>
+  );
+};
+
+export default ActivitiesPage;
