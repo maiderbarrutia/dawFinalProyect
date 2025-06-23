7666e9a4fe6eb8fd815a60cbd8e30cbc563056b1
Maider <maiderbarrutia@hotmail.com>
2025-06-23 18:27:57 +0200

    change location

--- frontend/src/hooks/useGoogleAnalytics.tsx
+++ frontend/src/hooks/useGoogleAnalytics.tsx
@@ -1,20 +1,17 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
-declare global {
-  interface Window {
-    gtag: (...args: any[]) => void;
-  }
-}
-
 const useGoogleAnalytics = (trackingId: string) => {
   const location = useLocation();
 
   useEffect(() => {
-    window.gtag('config', trackingId, {
-      page_path: location.pathname,
-    });
+    if (typeof window.gtag === 'function') {
+      window.gtag('config', trackingId, {
+        page_path: location.pathname,
+      });
+    }
   }, [location, trackingId]);
 };
 
 export default useGoogleAnalytics;
+
