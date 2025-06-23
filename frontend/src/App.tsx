389e56cf6168c92e2adfa882825a1f9f5bf565ae
Maider <maiderbarrutia@hotmail.com>
2025-06-23 18:35:32 +0200

    delete analytics gtag

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,6 @@
 import SiteRoutes from './routes/routes';
-// import useGoogleAnalytics from './hooks/useGoogleAnalytics';
 
 const App: React.FC = () => {
-  // useGoogleAnalytics('G-J9CSXZ53YK');
-  
   return (
     <SiteRoutes />
   );
